aabcff87d75f63c9760998f7e9791be94db00cdc
Haffiz Hissham <38602279+haffizhissham@users.noreply.github.com>
2023-08-25 09:35:57 +0800

    commit testing v2

--- todo-infra/bin/todo-infra.ts
+++ todo-infra/bin/todo-infra.ts
@@ -18,5 +18,5 @@ new TodoInfraStack(app, 'TodoInfraStack', {
   // env: { account: '123456789012', region: 'us-east-1' },
 
   /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
-  /* Test Commit */
+  /* Test Commit v2 */
 });
